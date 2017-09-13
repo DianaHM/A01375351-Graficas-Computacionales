236cdd9d882782c0d043e1fa627228ac82dfd1e9
DESKTOP-OT5FDVI\dihm1 <dihm.1011@gmail.com>
2017-09-13 12:01:31 -0500

    Clase 11/09/2017 Parte 2

--- Gráficas Computacionales/Main.cpp
+++ Gráficas Computacionales/Main.cpp
@@ -13,44 +13,39 @@ GLuint vao; //Identificador del manage al que vamos a asociar todos los VBOs
 
 GLuint shaderProgram; //Identificador del manager de los shaders (shaderProgram)
 
-float vertsPerFrame = 0.0f;
-float delta = 0.01f;
 
 void Initialize()
 {
 	std::vector<glm::vec2> positions;
-	
+
+	positions.push_back(glm::vec2(0.0f, 1.0f));
+	positions.push_back(glm::vec2(0.0f, 0.5f));
+	positions.push_back(glm::vec2(0.95f, 0.3f));
+	positions.push_back(glm::vec2(0.45f, 0.18f));
+	positions.push_back(glm::vec2(0.6f, -0.8f));
+	positions.push_back(glm::vec2(0.3f, -0.4f));
+	positions.push_back(glm::vec2(-0.6f, -0.8f));
+	positions.push_back(glm::vec2(-0.3f, -0.4f));
+	positions.push_back(glm::vec2(-0.95f, 0.3f));
+	positions.push_back(glm::vec2(-0.45f, 0.18));
+	positions.push_back(glm::vec2(0.0f, 1.0f));
+	positions.push_back(glm::vec2(0.0f, 0.5f));
+
 	std::vector<glm::vec3> colors;
 	
-	float r = 1.0;
-	float X = 1.0;
-	float Y = 0.0;
-	float contador = 0.0f;
-	
-	positions.push_back(glm::vec2(0.0f, 0.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
 	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
 
-	while (contador<=360.0f) {
-		
-		float angulo = glm::radians(contador);
-		float x = r*(glm::cos(angulo));
-		float y = r*(glm::sin(angulo));
-
-		positions.push_back(glm::vec2(x, y));
-		colors.push_back(glm::vec3(x, y, 0.0f));
-
-
-		contador = contador + 1.0f;
-		
-	
-		
-	}
-
-	
-	//Crear una nueva lista. Un color por cada vértice, en este caso 4 
-	
-	 
- 
 
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
@@ -75,7 +70,7 @@ void Initialize()
 
 	InputFile ifile; 	//Creamos un objeto para leer archivo 
 	
-	ifile.Read("DiscardCenter.vert"); //VERTEX SHADER. Leemos el archivo default.vert donde está el codigo del vertex shader 
+	ifile.Read("Default.vert"); //VERTEX SHADER. Leemos el archivo default.vert donde está el codigo del vertex shader 
 	std::string vertexSource = ifile.GetContents(); //Obtenemos el código fuente y lo guardamos en un string 
 	GLuint vertexShaderHandle = glCreateShader(GL_VERTEX_SHADER); //Creamos un shader de tipo vertex y guardamos su identificador en una variable
 	const GLchar*vertexSource_c = (const GLchar*)vertexSource.c_str(); //Obtener los datos en el formato correcto, en este caso en el lenguaje C
@@ -83,7 +78,7 @@ void Initialize()
 	glCompileShader(vertexShaderHandle); //Compilamos el shader en busca de errores 
 
 
-	ifile.Read("DiscardCenter.frag");
+	ifile.Read("Default.frag");
 	std::string fragmentSource = ifile.GetContents();
 	GLuint fragmentShaderHandle = glCreateShader(GL_FRAGMENT_SHADER);
 	const GLchar *fragmentSource_c = (const GLchar*)fragmentSource.c_str();
@@ -107,14 +102,10 @@ void GameLoop()
 	glUseProgram(shaderProgram); //Activamos el vertex shader y el fragment shaderl utilizando el manager 
 
 	glBindVertexArray(vao); //activamos el mamager y con esto se activan todos los VBOs asociados automaticamente
-	glDrawArrays(GL_TRIANGLE_FAN, 0, glm::clamp(vertsPerFrame, 0.0f, 362.0f)); //funcion de dibujando sin indices 
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 12); //funcion de dibujando sin indices 
 	glBindVertexArray(0); //terminamos de utilizar el manager 
 
 	glUseProgram(0);
-
-	vertsPerFrame += delta;
-	if (vertsPerFrame < 0.0f || vertsPerFrame >= 370.0f)
-		delta *= -1.0f;
 
 	//cuando terminamos de renderear cambiamos los buffers 
 	glutSwapBuffers();
@@ -128,11 +119,6 @@ void Idle()
 void ReshapeWindow(int width, int height)
 {
 	glViewport(0, 0, width, height);
-	glUseProgram(shaderProgram);
-	GLint uniformLocation =
-		glGetUniformLocation(shaderProgram, "Resolution");
-	glUniform2f(uniformLocation, width, height);
-	glUseProgram(0);
 
 }
 
@@ -152,6 +138,10 @@ int main(int argc, char* argv[])
 	glewInit();
 
 	glClearColor(1.0f, 1.0f, 0.5f, 1.0f);// la venta de OpenGL.
+	glEnable(GL_DEPTH_TEST); 
+	glEnable(GL_CULL_FACE); 
+	glCullFace(GL_BACK);
+
 	std::cout << glGetString(GL_VERSION) << std::endl;
 	Initialize();
 	glutMainLoop();
